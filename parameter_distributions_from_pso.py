306734760b7f924b3a0dda78a4cf0965f397e4ba
oscar <oo.ortega2247@gmail.com>
2016-05-04 14:00:06 -0500

    change to show distribution

--- parameter_distributions_from_pso.py
+++ parameter_distributions_from_pso.py
@@ -1,6 +1,9 @@
 import os
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
 
 def listdir_fullpath(d):
@@ -15,9 +18,11 @@ for i in data:parames.append(float(i[1]))
 
 all_pars = pd.DataFrame()
 for i, pat in enumerate(par_sets):
-    var = pd.read_table(pat, sep=',', names=['parameters','val'])
-    all_pars['par%d'%i] = var.val
+    var = pd.read_table(pat, sep=',', names=['parameters', 'val'])
+    all_pars['par%d' % i] = var.val
 all_pars.set_index(var.parameters, inplace=True)
 all_pars_t = all_pars.transpose()
-all_pars_t.hist(column='pore_transport_dissociate_BaxA_4_CytoCC_kc')
-all_pars
+
+plt.figure()
+all_pars_t['bind_L_R_to_LR_kr'].plot.hist()
+plt.show()
