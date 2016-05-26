cbc48967aae1e11db284244d3beeb2b0384bdad1
oscar <oo.ortega2247@gmail.com>
2016-05-26 15:12:05 -0500

    example change of parameters at different time points in tyson oscillator

---
+++ examples/tyson_oscillator_time_parameters_changed.py
@@ -0,0 +1,7 @@
+import numpy as np
+from miscellaneous_analysis import change_parameter_in_time
+from pysb.examples.tyson_oscillator import model
+
+tspan = np.linspace(0, 200, 200)
+
+change_parameter_in_time(model, tspan, 51, 5, ['kp4'], 2)
