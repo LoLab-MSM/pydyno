f8f7e163b30e91803d1b8f3eeb93794eb7b5dc29
oscar <oo.ortega2247@gmail.com>
2016-05-25 23:18:57 -0500

    changed read_par function to take vectors

--- helper_functions.py
+++ helper_functions.py
@@ -3,6 +3,7 @@ import csv
 import pandas as pd
 import re
 import numpy as np
+from collections import OrderedDict
 
 def listdir_fullpath(d):
     """Return a list of path of files in directory
@@ -26,7 +27,13 @@ def read_pars(par_path):
 
 
 def read_all_pars(pars_path):
-    par_sets = listdir_fullpath(pars_path)
+    if os.path.isfile(pars_path):
+        par_sets = pd.read_csv(pars_path, names=['parameters'])['parameters'].tolist()
+    elif os.path.isdir(pars_path):
+        par_sets = listdir_fullpath(pars_path)
+    else:
+        raise Exception("Not valid parameter file or path")
+
     all_pars = pd.DataFrame()
     for i, pat in enumerate(par_sets):
         var = pd.read_table(pat, sep=',', names=['parameters', 'val'])
@@ -36,7 +43,7 @@ def read_all_pars(pars_path):
     return all_pars_t
 
 
-def _parse_name(spec):
+def parse_name(spec):
     """Returns parsed name of species
 
         keyword arguments:
@@ -44,14 +51,26 @@ def _parse_name(spec):
     """
     m = spec.monomer_patterns
     lis_m = []
+    name_counts = OrderedDict()
+    parsed_name = ''
     for i in range(len(m)):
         tmp_1 = str(m[i]).partition('(')
         tmp_2 = re.findall(r"(?<=\').+(?=\')", str(m[i]))
+
         if not tmp_2:
             lis_m.append(tmp_1[0])
         else:
             lis_m.append(''.join([tmp_1[0], tmp_2[0]]))
-    return '_'.join(lis_m)
+
+    for name in lis_m:
+        name_counts[name] = lis_m.count(name)
+
+    for sp, counts in name_counts.items():
+        if counts == 1:
+            parsed_name += sp + '_'
+        else:
+            parsed_name += str(counts) + sp + '_'
+    return parsed_name[:len(parsed_name)-1]
 
 
 def _find_nearest_zero(array):
