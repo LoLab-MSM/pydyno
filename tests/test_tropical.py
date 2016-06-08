8a91bd55360770feaaf5e4e2f68f4061c392cb54
oscar <oo.ortega2247@gmail.com>
2016-06-08 00:52:48 -0500

    fixing tests

--- tests/test_tropical.py
+++ tests/test_tropical.py
@@ -1,14 +1,15 @@
 from pysb.examples.tyson_oscillator import model
-from tropicalize import run_tropical
+from tropicalize import Tropical
 from numpy import linspace
 from nose.tools import *
-import traceback
-import os
-import importlib
+
 
 t = linspace(0, 200, 200)
-tro = run_tropical(model, t)
+tro = Tropical(model)
+tro.tropicalize(t)
 
 
 def test_slaves():
     assert_equal(tro.passengers, [0, 1, 4])
+
+test_slaves()
