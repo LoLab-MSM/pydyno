a34b90467328278840c3ebe850b08c4fabf7ce0a
oscar <oo.ortega2247@gmail.com>
2016-06-08 00:38:04 -0500

    script to get drivers from different parameter sets

--- drivers_diff_parameters.py
+++ drivers_diff_parameters.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-from earm.lopez_embedded import model
 from tropicalize import run_tropical
 from multiprocessing import Pool
 from multiprocessing import cpu_count
@@ -11,16 +10,20 @@ import pandas
 import helper_functions as hf
 
 
-def all_parameters_signatures(par, model, tspan):
-    """Return tropical signature of parameter set
-
-        Keyword arguments:
-        par -- parameter file path
-        model -- PySB model
-        tspan -- time span
+def parameter_signatures(par, model, tspan):
     """
+
+    :param par: parameter file path or vector of parameters
+    :param model: PySB model
+    :param tspan: time span
+    :return: tropical signatures of input parameters
+    """
+
     try:
-        parames = hf.read_pars(par)
+        if isinstance(par, str):
+            parames = hf.read_pars(par)
+        else:
+            parames = par
         drivers = run_tropical(model, tspan, parameters=parames, sp_visualize=None)
         return drivers
     except:
@@ -29,39 +32,72 @@ def all_parameters_signatures(par, model, tspan):
         raise Exception("".join(traceback.format_exception(*sys.exc_info())))
 
 
-def compare_all_drivers_signatures(model, tspan, parameters_path, to_data_frame=False):
-    """Save a file with all the tropical signatures from the different parameter sets that equally fit the data.
-    If to_date_frame == True. It generates data frames of species that are drivers with all the parameter sets
-
-        Keyword arguments:
-        model -- PySB model
-        tspan -- time span
-        parameters_path -- list of parameter files path
-        to_data_frame -- optional, True if a data frame of species that are drivers with all parameter sets is wanted
+def compare_all_drivers_signatures(model, tspan, parameters, to_data_frame=False, dir_path=None):
     """
+
+    :param model: PySB model
+    :param tspan: time span
+    :param parameters: list of parameter files path
+    :param to_data_frame: optional, True if a data frame of species that are drivers with all parameter sets is wanted
+    :param dir_path: Path to directory where data frames are saved if to_data_frame=True
+    :return: Save a file with all the tropical signatures from the different parameter sets that equally fit the data.
+    If to_date_frame == True. It generates data frames of species that are drivers with all the parameter sets
+    """
+
     p = Pool(cpu_count() - 1)
-    all_drivers = p.map(functools.partial(all_parameters_signatures, model=model, tspan=tspan), parameters_path)
-    np.save("/home/oscar/tropical_project_new/drivers_all_parameters5", np.array(all_drivers))
+    all_drivers = p.map(functools.partial(parameter_signatures, model=model, tspan=tspan), parameters)
+    np.save("/home/oscar/Desktop/drivers_CORM", np.array(all_drivers))
 
     if to_data_frame:
-        tropical_data = all_drivers
-        drivers_all = [set(dr.keys()) for dr in tropical_data]
-        drivers_over_pars = set.intersection(*drivers_all)
-        drivers_to_df = {}
-        for sp in drivers_over_pars:
-            tmp = [0] * len(drivers_all)
-            for idx, tro in enumerate(tropical_data):
-                tmp[idx] = tro[sp]
-            drivers_to_df[sp] = tmp
-
-        for sp in drivers_to_df.keys():
-            pandas.DataFrame(np.array(drivers_to_df[sp]),
-                             index=parameters_path,
-                             columns=tspan[1:]).to_csv(
-                '/home/oscar/tropical_project_new/data_frames5/data_frame%d' % sp + '.csv')
+        array_to_dataframe(all_drivers, dir_path, tspan[1:], parameters)
     return
 
 
-t = np.linspace(0, 20000, 100)
-pars = hf.listdir_fullpath('/home/oscar/Documents/tropical_project/parameters_5')
-compare_all_drivers_signatures(model, t, pars, to_data_frame=True)
+def array_to_dataframe(array, dir_path, col_index=None, row_index=None):
+    """
+
+    :param array: array or path to npy file to convert to data frame
+    :param dir_path: Path to the directory where the data frames are saved
+    :param col_index: usually tspan
+    :param row_index: usually the name or idx of parameter set
+    :return:
+    """
+
+    if dir_path is not None:
+        path = dir_path
+    else:
+        raise Exception("'dir_path' must be given.")
+
+    if isinstance(array, str):
+        tropical_data = np.load(array)
+    else:
+        tropical_data = array
+
+    if col_index is not None:
+        cindex = col_index
+    else:
+        cindex = range(tropical_data.shape[1])
+
+    if row_index is not None:
+        rindex = row_index
+    else:
+        rindex = range(tropical_data.shape[0])
+
+    drivers_all = [set(dr.keys()) for dr in tropical_data]
+    drivers_over_pars = set.intersection(*drivers_all)
+    drivers_to_df = {}
+    for sp in drivers_over_pars:
+        tmp = [0] * len(drivers_all)
+        for idx, tro in enumerate(tropical_data):
+            tmp[idx] = tro[sp]
+        drivers_to_df[sp] = tmp
+
+    for sp in drivers_to_df.keys():
+        pandas.DataFrame(np.array(drivers_to_df[sp]),
+                         index=rindex,
+                         columns=cindex).to_csv(path + '/data_frame%d' % sp + '.csv')
+    return
+
+# t = np.linspace(0, 20000, 100)
+# pars = hf.listdir_fullpath('/home/oscar/Documents/tropical_project/parameters_5')
+# compare_all_drivers_signatures(model, t, pars, to_data_frame=True)
