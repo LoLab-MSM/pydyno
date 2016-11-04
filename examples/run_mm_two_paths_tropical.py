f857d0061c187f869b4f7f0169cfc33bee647b4b
oscar <oo.ortega2247@gmail.com>
2016-11-04 10:41:42 -0500

    updated import libraries

--- examples/run_mm_two_paths_tropical.py
+++ examples/run_mm_two_paths_tropical.py
@@ -1,8 +1,8 @@
 from mm_two_paths_model import model
-from tropicalize import run_tropical
+from max_plus_consumption_production import run_tropical
 import numpy as np
 
-tspan = np.linspace(0, 5, 51)
+tspan = np.linspace(0, 50, 501)
 
-run_tropical(model, tspan, sp_visualize=[3, 5])
+run_tropical(model, tspan, diff_par=0.5, type_sign='consumption', sp_visualize=[0, 5])
 
