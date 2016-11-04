7aef1da9e1c70dd49fd3d36c45fb584b526fa0b2
oscar <oo.ortega2247@gmail.com>
2016-11-04 10:43:00 -0500

    deleted comments, updated import libraries

--- examples/EARM/run_EARM_tropical.py
+++ examples/EARM/run_EARM_tropical.py
@@ -1,43 +1,15 @@
-import matplotlib
 from earm.lopez_embedded import model
-from tropicalize import run_tropical
+from max_plus_consumption_production import run_tropical
 import numpy as np
-import csv
-from pysb import *
-# tipe Bax cluster: 5400
-# type Bak cluster: 4052
+import os
+import helper_functions as hf
 
-f = open('/home/oscar/Documents/tropical_project/parameters_5000/pars_embedded_4052.txt')
-data = csv.reader(f)
-parames = [float(i[1]) for i in data]
+directory = os.path.dirname(__file__)
+parameters_path = os.path.join(directory, "parameters_5000")
+all_parameters = hf.listdir_fullpath(parameters_path)
+parameters = hf.read_pars(all_parameters[0])
 t = np.linspace(0, 20000,  100)
 
-run_tropical(model, t, parames, sp_visualize=[19])
-
-# import matplotlib.pyplot as plt
-# from pysb.integrate import odesolve
-#
-# y = odesolve(model, t, parames)
-#
-# p = model.parameters
-# index_kf_bak = p.index(p['pore_transport_complex_BakA_4_SmacM_kf'])
-# index_kf_bax = p.index(p['pore_transport_complex_BaxA_4_SmacM_kf'])
-# index_kr_bak = p.index(p['pore_transport_complex_BakA_4_SmacM_kr'])
-# index_kr_bax = p.index(p['pore_transport_complex_BaxA_4_SmacM_kr'])
-#
-# plt.figure(figsize=(12, 8))
-# plt.plot(t, y['__s20']*y['__s60']*parames[index_kf_bak], linewidth=2, label='s20*s60*pore_transport_complex_BakA_4_SmacM_kf')
-# plt.plot(t, y['__s20']*y['__s64']*parames[index_kf_bax], linewidth=2, label='s20*s64*pore_transport_complex_BaxA_4_SmacM_kf')
-# plt.plot(t, y['__s63']*parames[index_kr_bak], linewidth=2, label='s63*pore_transport_complex_BakA_4_SmacM_kr')
-# plt.plot(t, y['__s70']*parames[index_kr_bax], linewidth=2, label='s70*pore_transport_complex_BaxA_4_SmacM_kr')
-# plt.xlabel('Time(sec)', fontsize=18)
-# plt.ylabel('molecules', fontsize=18)
-# plt.title('SmacM monomials', fontsize=20)
-# plt.rc('legend',**{'fontsize':12})
-# plt.legend(loc=0)
-# plt.tick_params(axis='both', which='major', labelsize=14)
-# plt.savefig('/home/oscar/smac_bak.png', dpi=100, bbox_inches='tight')
-# plt.show()
-
+run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=[6])
 
 
