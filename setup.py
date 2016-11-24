9b7e513ec6d5fd526d5a80086f34601ac8101751
oscar <oo.ortega2247@gmail.com>
2016-11-23 18:45:45 -0600

    updated setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(
-    ext_modules = cythonize("choose_max.pyx")
-)
+
+def main():
+    setup(name='TroPy',
+          version='1.0',
+          description='Dynamic analysis of systems biology models',
+          author='Oscar Ortega',
+          author_email='oscar.ortega@vanderbilt.edu',
+          packages=['tropy'],
+          requires=['pysb'],
+          keywords=['systems', 'biology', 'model'],
+          ext_modules=cythonize("choose_max.pyx")
+          )
+
+if __name__ == '__main__':
+    main()
