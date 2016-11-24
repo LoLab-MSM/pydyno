ea37f1127c46970e462ca91d381c84fa943fee1f
oscar <oo.ortega2247@gmail.com>
2016-11-23 20:05:09 -0600

    updating file path

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from Cython.Build import cythonize
 
 
 def main():
-    setup(name='TroPy',
+    setup(name='tropical',
           version='1.0',
           description='Dynamic analysis of systems biology models',
           author='Oscar Ortega',
           author_email='oscar.ortega@vanderbilt.edu',
-          packages=['tropy'],
+          packages=['tropical'],
           requires=['pysb'],
           keywords=['systems', 'biology', 'model'],
-          ext_modules=cythonize("choose_max.pyx")
+          ext_modules=cythonize("tropical/choose_max.pyx")
           )
 
 if __name__ == '__main__':
