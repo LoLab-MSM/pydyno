9ba0222feaefa18ba6ff6290d4750bd33d9ead30
oscar <oo.ortega2247@gmail.com>
2016-12-10 19:28:12 -0600

    update import call

--- examples/run_tyson_oscillator_tropical.py
+++ examples/run_tyson_oscillator_tropical.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from pysb.examples.tyson_oscillator import model
-from tropical.max_plus_multiprocessing import run_tropical
+from tropical.max_plus_multiprocessing_numpy import run_tropical
 
 tspan = np.linspace(0, 200, 200)
-run_tropical(model, tspan, sp_visualize=[3,5])
+run_tropical(model, tspan, diff_par=.5, sp_visualize=[3, 5], find_passengers_by='imp_nodes', type_sign='production',
+             verbose=False)
