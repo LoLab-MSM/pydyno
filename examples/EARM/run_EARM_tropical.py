b99608996ef6a9266a4a6801ae6e2a6cda3eee33
oscar <oo.ortega2247@gmail.com>
2017-01-29 12:07:26 -0600

    updating import call to max_plus

--- examples/EARM/run_EARM_tropical.py
+++ examples/EARM/run_EARM_tropical.py
@@ -4,7 +4,7 @@ import tropical.helper_functions as hf
 import numpy as np
 
 from earm.lopez_embedded import model
-from tropical.max_plus_multiprocessing import run_tropical
+from tropical.max_plus_multiprocessing_numpy import run_tropical
 
 directory = os.path.dirname(__file__)
 parameters_path = os.path.join(directory, "parameters_5000")
@@ -12,4 +12,4 @@ all_parameters = hf.listdir_fullpath(parameters_path)
 parameters = hf.read_pars(all_parameters[0])
 t = np.linspace(0, 20000,  100)
 
-run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=[6])
+run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=None)
