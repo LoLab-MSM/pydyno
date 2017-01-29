f5c155c5de786ae39eb863e947fc90a6c263548a
oscar <oo.ortega2247@gmail.com>
2017-01-29 12:16:40 -0600

    simple enzyme reaction example

--- examples/run_mm_simple_tropical.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from mm_simple_model import model
-from tropical.max_plus_multiprocessing import run_tropical
-
-tspan = np.linspace(0, 50, 501)
-
-run_tropical(model, tspan, sp_visualize=None)
--- examples/simple_enzymatic/mm_simple_model.py
+++ examples/simple_enzymatic/mm_simple_model.py
@@ -34,16 +34,16 @@ Parameter('S0', 1000.*V)
 Initial(S(e=None), S0)
 
 
-tspan = np.linspace(0, 20, 100)
-y = odesolve(model, tspan)
-
-color = ['b', 'r', 'b', 'g']
-
-for i, j in enumerate(model.observables):
-    plt.figure()
-    plt.plot(tspan, y[j.name], label=j.name, linewidth=4, color=color[i])
-    plt.xlabel('time', fontsize=20)
-    plt.ylabel('Population', fontsize=20)
-    plt.legend(loc=0)
-    plt.savefig('/home/oscar/Desktop/mm%d' % i + '.eps')
-plt.show()
+# tspan = np.linspace(0, 20, 100)
+# y = odesolve(model, tspan)
+#
+# color = ['b', 'r', 'b', 'g']
+#
+# for i, j in enumerate(model.observables):
+#     plt.figure()
+#     plt.plot(tspan, y[j.name], label=j.name, linewidth=4, color=color[i])
+#     plt.xlabel('time', fontsize=20)
+#     plt.ylabel('Population', fontsize=20)
+#     plt.legend(loc=0)
+#     plt.savefig('/home/oscar/Desktop/mm%d' % i + '.eps')
+# plt.show()
