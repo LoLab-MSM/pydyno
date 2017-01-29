c2789530d25bf22182023ab81d160e6929ffb8da
oscar <oo.ortega2247@gmail.com>
2017-01-29 11:52:11 -0600

    updating cluster_analysis call for the arguments

--- examples/EARM/earm_ic_sp37.py
+++ examples/EARM/earm_ic_sp37.py
@@ -1,6 +1,7 @@
 from earm.lopez_embedded import model
 import numpy as np
 from tropical.analysis_of_clusters import AnalysisCluster
+from tropical.helper_functions import sig_apop
 
 all_parameters = np.load('/home/oscar/Documents/tropical_earm_IC/IC_10000_parameters_consumption.npy')
 t = np.linspace(0, 20000, 100)
@@ -15,7 +16,8 @@ clusters = ['/home/oscar/Documents/tropical_earm_IC/bid_clusteredPAM_pars_consum
             '/home/oscar/Documents/tropical_earm_IC/bid_clusteredPAM_pars_consumption/' \
             'data_frame37_Type4'
             ]
-
-a = AnalysisCluster(model, t, all_parameters, clusters)
-a.plot_sp_IC_distributions([55, 56, 57], '/home/oscar/Desktop')
-# a.plot_dynamics_cluster_types([37], save_path='/home/oscar/Desktop', ic_idx=[55])
+simulations_result = np.load('/home/oscar/Desktop/all_simulations.npy')
+a = AnalysisCluster(model, t, all_parameters, clusters, sim_results=simulations_result)
+# a.plot_sp_IC_distributions([55, 56, 57], '/home/oscar/Desktop')
+a.plot_dynamics_cluster_types([39], save_path='/home/oscar/Desktop', species_to_fit=[39], fit_ftn=sig_apop, ic_idx=[23],
+                              **{'p0': [100, 100, 100]})
