c2b3fc828e21a2721c350e4150555fa7e45b291a
oscar <oo.ortega2247@gmail.com>
2017-03-17 16:35:18 -0500

    added tests to tropy

--- tests/test_tropical.py
+++ tests/test_tropical.py
@@ -1,16 +1,17 @@
 import numpy as np
-
+from nose.tools import *
 from pysb.examples.tyson_oscillator import model
 from pysb.simulator import ScipyOdeSimulator
 from pysb.simulator.base import SimulatorException
-from tropical.max_plus_multiprocessing import Tropical, run_tropical
+from tropical.max_plus_multiprocessing_numpy import Tropical, run_tropical
 
 
 class TestFluxVisualization(object):
     def setUp(self):
         self.model = model
         self.time = np.linspace(0, 200, 200)
-        self.sim = ScipyOdeSimulator.execute(self.model, tspan=self.time)
+        self.sim = ScipyOdeSimulator(self.model, tspan=self.time).run()
+        self.y = self.sim.all
 
     def tearDown(self):
         self.model = None
@@ -21,34 +22,45 @@ class TestFluxVisualization(object):
         tropical = Tropical(self.model)
         tropical.tropicalize(tspan=self.time)
 
-    def test_verbpse(self):
+    def test_verbose(self):
         tropical_verbose = Tropical(self.model)
         tropical_verbose.tropicalize(tspan=self.time, verbose=True)
 
-    def test_visualization(self):
-        tropical_visualization = Tropical(self.model)
-        tropical_visualization.tropicalize(tspan=self.time)
-        tropical_visualization.visualization(driver_species=[3, 5])
+    def test_type_sign_production(self):
+        tropical_production = Tropical(self.model)
+        tropical_production.tropicalize(tspan=self.time, type_sign='production')
 
-    def test_get_trop_data(self):
-        tropical_data = Tropical(self.model)
-        tropical_data.tropicalize(tspan=self.time)
-        tropical_data.get_trop_data()
+    def test_type_sign_coonsumption(self):
+        tropical_consumption = Tropical(self.model)
+        tropical_consumption.tropicalize(tspan=self.time, type_sign='consumption')
+
+    @raises(ValueError)
+    def test_type_sign_wrong(self):
+        tropical_wrong_type = Tropical(self.model)
+        tropical_wrong_type.tropicalize(tspan=self.time, type_sign='bla')
+
+    def test_find_passengers_imp(self):
+        tropical_find_passengers_imp = Tropical(self.model)
+        tropical_find_passengers_imp.tropicalize(tspan=self.time, find_passengers_by='imp_nodes')
+
+    def test_find_passengers_qssa(self):
+        tropical_find_passengers_qssa = Tropical(self.model)
+        tropical_find_passengers_qssa.tropicalize(tspan=self.time, find_passengers_by='qssa')
+
+    @raises(Exception)
+    def test_find_passenger_wrong_type(self):
+        tropical_wrong_type = Tropical(self.model)
+        tropical_wrong_type.tropicalize(tspan=self.time, find_passengers_by='bla')
+
+    # def test_visualization(self):
+    #     tropical_visualization = Tropical(self.model)
+    #     tropical_visualization.tropicalize(tspan=self.time)
+    #     tropical_visualization.visualization(driver_species=[3, 5])
 
     def test_get_passenger(self):
         tropical_passenger = Tropical(self.model)
         tropical_passenger.tropicalize(tspan=self.time)
         tropical_passenger.get_passenger()
-
-    def test_get_tropical_eq(self):
-        tropical_eq = Tropical(self.model)
-        tropical_eq.tropicalize(tspan=self.time)
-        tropical_eq.get_tropical_eqs()
-
-    def test_get_driver_signatures(self):
-        tropical_signatures = Tropical(self.model)
-        tropical_signatures.tropicalize(tspan=self.time)
-        tropical_signatures.get_driver_signatures()
 
     def test_run_tropical(self):
         run_tropical(self.model, self.time)
@@ -56,10 +68,6 @@ class TestFluxVisualization(object):
     def test_plot_imposed_trace(self):
         tropical_trace = Tropical(self.model)
         tropical_trace.tropicalize(tspan=self.time, plot_imposed_trace=True)
-
-    def test_list_parameters(self):
-        tropical_paramaters = Tropical(self.model)
-        tropical_paramaters.tropicalize(tspan=self.time, param_values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
     @raises(SimulatorException)
     def test_time_not_defined(self):
@@ -70,7 +78,6 @@ class TestFluxVisualization(object):
     def test_parameters_different_length(self):
         tropical_pars = Tropical(self.model)
         tropical_pars.tropicalize(tspan=self.time, param_values=[1, 2])
-
 
 
 
