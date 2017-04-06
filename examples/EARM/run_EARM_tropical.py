04cdd04251387b1231bd68c77dfdb6f5f21fbfe7
oscar <oo.ortega2247@gmail.com>
2017-04-06 17:04:40 -0500

    organized import calls

--- examples/EARM/run_EARM_tropical.py
+++ examples/EARM/run_EARM_tropical.py
@@ -1,8 +1,6 @@
 import os
-
 import tropical.helper_functions as hf
 import numpy as np
-
 from earm.lopez_embedded import model
 from tropical.max_plus_multiprocessing_numpy import run_tropical
 
@@ -12,4 +10,4 @@ all_parameters = hf.listdir_fullpath(parameters_path)
 parameters = hf.read_pars(all_parameters[0])
 t = np.linspace(0, 20000,  100)
 
-run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=None)
+run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=[37])
