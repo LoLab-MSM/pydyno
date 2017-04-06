90888415d6f930bca5b35725f41f3900a7e1177e
oscar <oo.ortega2247@gmail.com>
2017-04-06 17:07:14 -0500

    updated import calls

--- examples/EARM/run_earm1_3.py
+++ examples/EARM/run_earm1_3.py
@@ -1,7 +1,6 @@
 import numpy as np
-
+from tropical.max_plus_multiprocessing_numpy import run_tropical
 from pysb.examples.earm_1_3 import model
-from tropical.max_plus_multiprocessing import run_tropical
 
 t = np.linspace(0, 20000,  100)
 
