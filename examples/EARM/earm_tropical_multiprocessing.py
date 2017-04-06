3e5775f24d85d249c264628b5f0029c8499358d2
oscar <oo.ortega2247@gmail.com>
2017-04-06 17:43:54 -0500

    updated import calls

--- examples/EARM/earm_tropical_multiprocessing.py
+++ examples/EARM/earm_tropical_multiprocessing.py
@@ -1,14 +1,12 @@
 import os
-
-import tropical.helper_functions as hf
 import numpy as np
-
+import tropical.helper_functions as hf
 from earm.lopez_embedded import model
-from tropical.max_plus_multiprocessing import run_tropical_multiprocessing
+from tropical.max_plus_global import run_tropical_multiprocessing
 
 directory = os.path.dirname(__file__)
 pars_path = os.path.join(directory, 'parameters_5000')
 pars = hf.listdir_fullpath(pars_path)
 t = np.linspace(0, 20000, 100)
 
-run_tropical_multiprocessing(model, t, pars[:10])
+run_tropical_multiprocessing(model, t, pars[:10], global_signature=True, dir_path=directory)
