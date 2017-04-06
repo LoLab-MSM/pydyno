95daa4efce69beb650ec8031505e757e4f869cc1
oscar <oo.ortega2247@gmail.com>
2017-04-06 17:45:32 -0500

    change parameter value in function

--- examples/tyson_oscillator/run_tyson_oscillator_tropical.py
+++ examples/tyson_oscillator/run_tyson_oscillator_tropical.py
@@ -1,8 +1,7 @@
 import numpy as np
-
 from pysb.examples.tyson_oscillator import model
-from tropical.max_plus_multiprocessing_numpy import run_tropical
+from tropical.max_plus_global import run_tropical
 
 tspan = np.linspace(0, 100, 100)
-run_tropical(model, tspan, diff_par=.5, sp_visualize=[3, 5], find_passengers_by='qssa', plot_imposed_trace=True,
-             type_sign='production', verbose=False)
+run_tropical(model, tspan, diff_par=.5, sp_visualize=None, find_passengers_by='imp_nodes', plot_imposed_trace=True,
+             global_signature=True, type_sign='production', verbose=False)
