aca10c61f593d2d35cbf83391dcb3345399a3f58
oscar <oo.ortega2247@gmail.com>
2017-07-14 14:44:05 -0500

    re arranged functions location

--- tropical/helper_functions.py
+++ tropical/helper_functions.py
@@ -1,11 +1,13 @@
 import os
-import csv
 import pandas as pd
 import re
 import numpy as np
 from collections import OrderedDict
 from pysb.bng import generate_equations
 import pysb
+from scipy.optimize import curve_fit
+from pysb.simulator import ScipyOdeSimulator
+from itertools import compress
 
 
 def listdir_fullpath(d):
@@ -154,6 +156,102 @@ def sig_apop(t, f, td, ts):
     ts -- is the switching time between initial and complete effector substrate  cleavage
     """
     return f - f / (1 + np.exp((t - td) / (4 * ts)))
+
+
+def check_param_values(model, param_values):
+    if param_values is not None:
+        if type(param_values) is str:
+            pars_to_check = read_pars(param_values)
+        else:
+            pars_to_check = param_values
+        # accept vector of parameter values as an argument
+        if len(pars_to_check) != len(model.parameters):
+            raise Exception("param_values must be the same length as model.parameters")
+        # convert model parameters into dictionary
+        pars_checked = dict((p.name, pars_to_check[i]) for i, p in enumerate(model.parameters))
+    else:
+        # create parameter vector from the values in the model
+        pars_checked = dict((p.name, p.value) for i, p in enumerate(model.parameters))
+    return pars_checked
+
+
+def pre_equilibration(model, time_search, ligand_par_name, ligand_idx, ligand_value=None, parameters=None, tolerance=1e-6):
+    """
+
+    :param model: PySB model
+    :param ligand_par_name: Species whose value want to be changed.
+    :param time_search: time span array to be used to find the equilibrium
+    :param tolerance: (tolerance, -tolerance) Range within equilibrium is considered as reached
+    :param ligand_value: Initial condition of ligand (usually zero)
+    :param parameters: Model parameters, must have same order as model.parameters
+    :return:
+    """
+    param_dict = parameters
+    # Check if ligand name to be used for pre equilibration is provided
+    if not isinstance(ligand_par_name, str):
+        raise Exception('ligand must be a string with the parameter name')
+
+    ligand_prev = param_dict[ligand_par_name]
+
+    if ligand_value is not None:
+        param_dict[ligand_par_name] = ligand_value
+    else:
+        param_dict[ligand_par_name] = 0
+
+    # Solve system for the time span provided
+    solver = ScipyOdeSimulator(model, tspan=time_search, param_values=param_dict).run()
+    y = solver.species.T
+    dt = time_search[1] - time_search[0]
+
+    time_to_equilibration = [0, 0]
+    for idx, sp in enumerate(y):
+        sp_eq = False
+        derivative = np.diff(sp) / dt
+        derivative_range = ((derivative < tolerance) & (derivative > -tolerance))
+        # Indexes of values less than tolerance and greater than -tolerance
+        derivative_range_idxs = list(compress(range(len(derivative_range)), derivative_range))
+        for i in derivative_range_idxs:
+            # Check if derivative is close to zero in the time points ahead
+            if (derivative[i + 3] < tolerance) | (derivative[i + 3] > -tolerance):
+                sp_eq = True
+                if time_search[i] > time_to_equilibration[0]:
+                    time_to_equilibration[0] = time_search[i]
+                    time_to_equilibration[1] = i
+            if not sp_eq:
+                raise Exception('Equilibrium can not be reached within the time_search input')
+            if sp_eq:
+                break
+        else:
+            raise Exception('Species s{0} has not reached equilibrium'.format(idx))
+
+    y[ligand_idx, time_to_equilibration[1]] = ligand_prev
+    conc_eq = y[:, time_to_equilibration[1]]
+    return time_to_equilibration, conc_eq
+
+
+def truncate_signature_death(simulations, signatures, species_to_fit):
+    if type(simulations) == str:
+        all_simulations = np.load(simulations)
+    else:
+        all_simulations = simulations
+    if type(signatures) == str:
+       snts = pd.read_csv(signatures, header=0, index_col=0)
+    else:
+        snts = signatures
+
+    tspan = snts.columns.values
+    tspan = tspan.astype(np.float)
+
+    for i, y in enumerate(all_simulations):
+        try:
+            td = curve_fit(f=sig_apop, xdata=tspan, ydata=y[species_to_fit], p0=[100, 100, 100])[0][1]
+        except:
+            print "Trajectory {0} can't be fitted".format(i)
+        idx_death = min(enumerate(tspan), key=lambda x: abs(x[1]-td))[0]
+        snts.loc[i][idx_death:len(tspan)] = 'dead'
+
+    snts.to_csv(signatures)
+
 
 
 def sps_signature_to_df(signatures, dir_path, global_signature=False, col_index=None, row_index=None):
