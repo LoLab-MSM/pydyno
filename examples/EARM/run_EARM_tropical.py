65efa189b5eb60c21a2da957b455bcdd38665b7a
oscar <oo.ortega2247@gmail.com>
2017-07-27 12:53:34 -0500

    range function to get tropical signatures

--- examples/EARM/run_EARM_tropical.py
+++ examples/EARM/run_EARM_tropical.py
@@ -2,12 +2,19 @@ import os
 import tropical.helper_functions as hf
 import numpy as np
 from earm.lopez_embedded import model
-from tropical.max_plus_multiprocessing_numpy import run_tropical
+# from tropical.max_plus_multiprocessing_numpy import run_tropical
+from tropical.max_plus_global_new import run_tropical
 
 directory = os.path.dirname(__file__)
 parameters_path = os.path.join(directory, "parameters_5000")
 all_parameters = hf.listdir_fullpath(parameters_path)
-parameters = hf.read_pars(all_parameters[0])
+parameters = hf.read_pars(all_parameters[237])
+# parameters[58] = 0
+# parameters[63] = 0
+# parameters[56] = 0
 t = np.linspace(0, 20000,  100)
 
-run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=[37])
+# run_tropical(model, t, parameters, diff_par=1, type_sign='consumption', sp_visualize=[37])
+a=run_tropical(model, t, parameters, diff_par=0.1, type_sign='consumption', global_signature=False,
+             pre_equilibrate=False, sp_visualize=[37])
+
