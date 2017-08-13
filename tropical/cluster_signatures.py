1afe693dac88a2b19ff952293986c0511e1993f5
oscar <oo.ortega2247@gmail.com>
2017-08-13 12:50:23 -0500

    functions to use longest common subsequence metric and plot cluster of sequences

---
+++ tropical/cluster_signatures.py
@@ -0,0 +1,124 @@
+from __future__ import division
+import mlpy
+import pandas as pd
+from sklearn.metrics.pairwise import pairwise_distances
+import sklearn.cluster as cluster
+import numpy as np
+from sklearn import metrics
+from pam_clustering import kMedoids
+import colorsys
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from collections import OrderedDict
+import random
+import hdbscan
+from scipy import stats
+from distinct_colors import distinct_colors
+
+signatures_path = '/Users/dionisio/Documents/PhD/tropical_earm_IC/signatures_within_range_function/' \
+                  'signatures_all_kpars_consumption_nums/data_frame37_copy.csv'
+X = pd.read_csv(signatures_path, header=0, index_col=0).as_matrix()[:, 0:50]
+unique_sequences, indices, seq_weigths = np.unique(X, return_counts=True, return_inverse=True, axis=0)
+
+
+def lcs_distance2(seq1, seq2):
+    return mlpy.lcs_std(seq1, seq1)[0] + mlpy.lcs_std(seq2, seq2)[0] - 2*mlpy.lcs_std(seq1, seq2)[0]
+
+
+def lcs_distance(seq1, seq2):
+    seq_len = len(seq1)
+    return 2 * seq_len - 2 * mlpy.lcs_std(seq1, seq2)[0]
+
+diss = pairwise_distances(unique_sequences, metric=lcs_distance, n_jobs=1)
+
+## HDBSCAN
+hdb = hdbscan.HDBSCAN(min_cluster_size=50, min_samples=15, metric='precomputed').fit(diss)
+
+
+## PAM
+pam1 = cluster.KMedoids(distance_metric=lcs_distance, n_clusters=10).fit(unique_sequences)
+pam2 = kMedoids(diss, 10)[1]
+
+pam1_labels = pam1.labels_
+pam2_labels = np.zeros(shape=len(unique_sequences), dtype=np.int)
+
+for clus, seqs_idx in pam2.items():
+    pam2_labels[seqs_idx] = int(clus)
+
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X=diss, labels=pam1.labels_, metric='precomputed'))
+
+
+# plot function
+unique_states = np.unique(unique_sequences)
+N = len(unique_states)
+
+# function to get n different colors
+# colores = [colorsys.hsv_to_rgb(x*1.0/N, 0.5, 0.7) for x in range(N)]
+# random.shuffle(colores)
+
+colores = distinct_colors(N)
+
+states_color_map = OrderedDict((state, colores[x]) for x, state in enumerate(unique_states))
+cmap = ListedColormap(states_color_map.values())
+bounds = states_color_map.keys()
+norm = BoundaryNorm(bounds, cmap.N)
+
+import matplotlib.patches as mpatches
+
+def plot_sequences(all_sequences, cluster_labels, type_plot='all'):
+    """
+
+    :param all_sequences:
+    :param cluster_labels:
+    :param type_plot: it can be all, mode
+    :return:
+    """
+    sil_samples = metrics.silhouette_samples(X=diss, labels=cluster_labels, metric='precomputed')
+    if type_plot == 'mode':
+        for i in set(cluster_labels):
+            plt.figure()
+            clus_seqs = all_sequences[cluster_labels == i]
+            seqs_in_cluster = clus_seqs.shape[0]
+            modal_states, mode_counts = stats.mode(clus_seqs, axis=0)
+            mc_norm = np.divide(mode_counts[0], seqs_in_cluster, dtype=np.float)
+            ind = np.array(range(clus_seqs.shape[1]))
+
+            colors = [states_color_map[c] for c in modal_states[0]]
+            legend_patches = [mpatches.Patch(color=states_color_map[c], label=c) for c in set(modal_states[0])]
+            plt.bar(ind, mc_norm, color=colors, label=modal_states[0], width=1)
+            plt.legend(handles=legend_patches)
+
+    else:
+        for i in set(cluster_labels):
+            clus_seqs = all_sequences[cluster_labels == i]
+            clus_sil_samples = sil_samples[cluster_labels == i]
+            clus_sil_sort = np.argsort(clus_sil_samples)
+            clus_seqs = clus_seqs[clus_sil_sort]
+            xx = np.array(range(clus_seqs.shape[1] + 1))
+
+            plt.figure()
+            for idx, seq in enumerate(clus_seqs):
+                y = np.array([idx]*(clus_seqs.shape[1] + 1))
+                points = np.array([xx, y]).T.reshape(-1, 1, 2)
+                segments = np.concatenate([points[:-1], points[1:]], axis=1)
+                lc = LineCollection(segments, cmap=cmap, norm=norm)
+                lc.set_array(seq)
+                lc.set_linewidth(10)
+                plt.gca().add_collection(lc)
+
+            # ax = plt.axes()
+            # ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+            plt.title('Cluster {0}'.format(i))
+            plt.xlabel('Time')
+            plt.ylabel('Sequences')
+            plt.xlim(xx.min(), xx.max())
+            plt.ylim(0, len(clus_seqs))
+            plt.show()
+
+# plot_sequences(unique_sequences, hdb.labels_, type_plot='mode')
+plot_sequences(unique_sequences, pam1_labels)
+# plot_sequences(unique_sequences, pam2_labels)
+
+
