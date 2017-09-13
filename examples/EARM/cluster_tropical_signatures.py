06094e1282caea399d91fc532d968b88d18830c6
oscar <oo.ortega2247@gmail.com>
2017-09-13 11:36:59 -0500

    examples to cluster other species

--- examples/EARM/cluster_tropical_signatures.py
+++ examples/EARM/cluster_tropical_signatures.py
@@ -1,11 +1,17 @@
 from tropical import clustering
 
 
-signatures_path = '/Users/dionisio/Documents/PhD/tropical_earm_IC/signatures_within_range_function/' \
+signatures_path37 = '/Users/dionisio/Documents/PhD/tropical_earm_IC/signatures_within_range_function/' \
                   'signatures_all_kpars/data_frames/data_frame37.csv'
 
-a = clustering.ClusterSequences(data=signatures_path, truncate_seq=50)
+signatures_path3 = '/Users/dionisio/Documents/PhD/tropical_earm_IC/signatures_within_range_function/' \
+                  'signatures_all_kpars/data_frames/data_frame3.csv'
+
+signatures_path6 = '/Users/dionisio/Documents/PhD/tropical_earm_IC/signatures_within_range_function/' \
+                  'signatures_all_kpars/data_frames/data_frame6.csv'
+
+a = clustering.ClusterSequences(data=signatures_path6, truncate_seq=50)
 a.diss_matrix()
-a.hdbscan()
+a.hdbscan(min_samples=10)
 b = clustering.PlotSequences(a)
-b.modal_plot()
+b.all_trajectories_plot()
