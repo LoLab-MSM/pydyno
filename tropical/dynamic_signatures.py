76a2479961690165925f17f393844e3a40f57e52
oscar <oo.ortega2247@gmail.com>
2017-11-15 15:29:28 -0600

    added dynamic signatures of how species are produced

--- tropical/dynamic_signatures.py
+++ tropical/dynamic_signatures.py
@@ -1,13 +1,19 @@
 import tropical.helper_functions as hf
-import h5py
 from pysb.simulator import SimulationResult, ScipyOdeSimulator, CupSodaSimulator
-import collections
 import numpy
 import operator
 import sympy
 import math
-from pathos.multiprocessing import ProcessingPool as Pool
 import itertools
+try:
+    from pathos.multiprocessing import ProcessingPool as Pool
+except ImportError:
+    Pool = None
+
+try:
+    import h5py
+except ImportError:
+    h5py = None
 
 
 class Tropical(object):
@@ -17,46 +23,21 @@ class Tropical(object):
         self.all_comb = {}
         self.model = model
         self.par_name_idx = {j.name: i for i, j in enumerate(self.model.parameters)}
-        self.trajectories = []
         self._is_setup = False
         self.passengers = []
         self.eqs_for_tropicalization = {}
-        self.tspan = []
+        self.diff_par = None
+        self.tspan = None
 
-    def setup_tropical(self, sim_or_params, tspan=None, simulator=None, passengers_by='imp_nodes'):
-        self.get_simulations(sim_or_params=sim_or_params, tspan=tspan, simulator=simulator)
+    def setup_tropical(self, tspan, diff_par=1, passengers_by='imp_nodes'):
         self.get_passengers(by=passengers_by)
+        self.diff_par = diff_par
+        self.tspan = tspan
         self.equations_to_tropicalize()
         self.set_combinations_sm()
         self._is_setup = True
         return
 
-    def get_simulations(self, sim_or_params, tspan=None, simulator=None):
-        if isinstance(sim_or_params, str):
-            if h5py.is_hdf5(sim_or_params):
-                sim = SimulationResult.load(sim_or_params)
-                tspan = sim.tout[0]
-            else:
-                raise TypeError('File format not supported')
-        elif isinstance(sim_or_params, SimulationResult):
-            sim = sim_or_params
-            tspan = sim.tout[0]
-        elif isinstance(sim_or_params, collections.Iterable) or sim_or_params is None:
-            # TODO check parameter length
-            parameters = sim_or_params
-            if simulator == 'scipy':
-                sim = ScipyOdeSimulator(model=self.model, tspan=tspan, param_values=parameters).run()
-            elif simulator == 'cupsoda':
-                sim = CupSodaSimulator(model=self.model, tspan=tspan, param_values=parameters).run()
-            else:
-                raise ValueError(' A valid simulator must be provided')
-        else:
-            raise TypeError('format not supported')
-
-        self.trajectories = sim.all
-        self.parameters = sim.param_values
-        self.tspan = tspan
-        return
 
     def get_passengers(self, by='imp_nodes'):
         if by == 'imp_nodes':
@@ -90,7 +71,6 @@ class Tropical(object):
         :return:
         """
         mons_pos_neg = [numpy.where(array > 0)[0], numpy.where(array < 0)[0]]
-        # print (mons_pos_neg)
         signs = [1, -1]
         ascending_order = [False, True]
         mons_types = ['products', 'reactants']
@@ -135,8 +115,55 @@ class Tropical(object):
                     break
             pos_neg_largest[ii] = largest_prod
             # print(mon_type, mon_names_ready, mon_comb_type, largest_prod)
-        # print (pos_neg_largest, mon_names)
         return pos_neg_largest
+
+    def signature(self, y, param_values):
+        # Dictionary that will contain the signature of each of the species to study
+        if not self._is_setup:
+            raise Exception('you must setup tropical first')
+        all_signatures = {}
+        for sp in self.eqs_for_tropicalization:
+            # reaction terms for positive and negative monomials
+            monomials = []
+            for term in self.model.reactions_bidirectional:
+                total_rate = 0
+                for mon_type, mon_sign in zip(['products', 'reactants'], [1, -1]):
+                    if sp in term[mon_type]:
+                        count = term[mon_type].count(sp)
+                        total_rate = total_rate + (mon_sign * count * term['rate'])
+                if total_rate == 0:
+                    continue
+                monomials.append(total_rate)
+
+            # Dictionary whose keys are the symbolic monomials and the values are the simulation results
+            mons_dict = {}
+            for mon_p in monomials:
+                mon_p_values = mon_p
+
+                if mon_p_values == 0:
+                    mons_dict[mon_p] = [0] * len(self.tspan)
+                else:
+                    var_prod = [atom for atom in mon_p_values.atoms(sympy.Symbol)]  # Variables of monomial
+                    arg_prod = [0] * len(var_prod)
+                    for idx, va in enumerate(var_prod):
+                        if str(va).startswith('__'):
+                            arg_prod[idx] = numpy.maximum(self.mach_eps, y[str(va)])
+                        else:
+                            arg_prod[idx] = param_values[self.par_name_idx[va.name]]
+                    # arg_prod = [numpy.maximum(self.mach_eps, y[str(va)]) for va in var_prod]
+                    f_prod = sympy.lambdify(var_prod, mon_p_values)
+                    prod_values = f_prod(*arg_prod)
+                    mons_dict[mon_p] = prod_values
+            mons_names = {}
+            mons_array = numpy.zeros((len(mons_dict.keys()), len(self.tspan)))
+            for idx, name in enumerate(mons_dict.keys()):
+                mons_array[idx] = mons_dict[name]
+                mons_names[name] = idx
+
+            signature_species = numpy.apply_along_axis(self.choose_max_pos_neg, 0, mons_array,
+                                                       *(mons_names, self.diff_par, self.all_comb[sp]))
+            all_signatures[sp] = list(signature_species)
+        return all_signatures
 
     def set_combinations_sm(self, max_comb=None, create_sm=False):
 
@@ -194,59 +221,48 @@ class Tropical(object):
             self.all_comb[sp] = pos_neg_combs
         return
 
-    def signature(self, y, pars_ready, diff_par=1):
-        # Dictionary that will contain the signature of each of the species to study
-        if not self._is_setup:
-            raise Exception('you must setup tropical first')
-        all_signatures = {}
 
-        for sp in self.eqs_for_tropicalization:
-            # reaction terms for positive and negative monomials
-            monomials = []
-            for term in self.model.reactions_bidirectional:
-                total_rate = 0
-                for mon_type, mon_sign in zip(['products', 'reactants'], [1, -1]):
-                    if sp in term[mon_type]:
-                        count = term[mon_type].count(sp)
-                        total_rate = total_rate + (mon_sign * count * term['rate'])
-                if total_rate == 0:
-                    continue
-                monomials.append(total_rate)
-            # Dictionary whose keys are the symbolic monomials and the values are the simulation results
-            mons_dict = {}
-            for mon_p in monomials:
-                mon_p_values = mon_p
-                # TODO Figure out a way that doesnt require an if statement here
-                if mon_p_values == 0:
-                    mons_dict[mon_p] = [0] * len(self.tspan)
-                else:
-                    var_prod = [atom for atom in mon_p_values.atoms(sympy.Symbol)]  # Variables of monomial
-                    arg_prod = [0] * len(var_prod)
-                    for idx, va in enumerate(var_prod):
-                        if str(va).startswith('__'):
-                            arg_prod[idx] = numpy.maximum(self.mach_eps, y[str(va)])
-                        else:
-                            arg_prod[idx] = pars_ready[self.par_name_idx[va.name]]
-                    # arg_prod = [numpy.maximum(self.mach_eps, y[str(va)]) for va in var_prod]
-                    f_prod = sympy.lambdify(var_prod, mon_p_values)
-
-                    prod_values = f_prod(*arg_prod)
-                    mons_dict[mon_p] = prod_values
-            mons_names = {}
-            mons_array = numpy.zeros((len(mons_dict.keys()), len(self.tspan)))
-            for idx, name in enumerate(mons_dict.keys()):
-                mons_array[idx] = mons_dict[name]
-                mons_names[name] = idx
-            signature_species = numpy.apply_along_axis(self.choose_max_pos_neg, 0, mons_array,
-                                                       *(mons_names, diff_par, self.all_comb[sp]))
-            # print (sp, signature_species)
-            all_signatures[sp] = list(signature_species)
-        return all_signatures
+def get_simulations(simulations):
+    if isinstance(simulations, str):
+        if h5py is None:
+            raise Exception('please install the h5py package for this feature')
+        if h5py.is_hdf5(simulations):
+            sim = SimulationResult.load(simulations)
+            tspan = sim.tout[0]
+        else:
+            raise TypeError('File format not supported')
+    elif isinstance(simulations, SimulationResult):
+        sim = simulations
+        tspan = sim.tout[0]
+    else:
+        raise TypeError('format not supported')
+    trajectories = sim.all
+    parameters = sim.param_values
+    nsims = sim.nsims
+    tspan = tspan
+    return trajectories, parameters, nsims, tspan
 
 
-def run_tropical(model, sim_or_params=None, tspan=None, simulator=None, passengers_by='imp_nodes', diff_par=1, cpu_cores=1):
+def run_tropical(model, simulations=None, passenger_by='imp_nodes', diff_par=1):
+    trajectories, parameters, nsims, tspan = get_simulations(simulations)
     tro = Tropical(model)
-    tro.setup_tropical(sim_or_params, tspan=tspan, simulator=simulator, passengers_by=passengers_by)
+    tro.setup_tropical(tspan=tspan, diff_par=diff_par, passengers_by=passenger_by)
+    signatures = tro.signature(y=trajectories, param_values=parameters[0])
+    return signatures
+
+
+def run_tropical_multi(model, simulations=None, passengers_by='imp_nodes', diff_par=1, cpu_cores=1):
+    if Pool is None:
+        raise Exception('Please install the pathos package for this feature')
+
+    trajectories, parameters, nsims, tspan = get_simulations(simulations)
+    tro = Tropical(model)
+
+    tro.setup_tropical(tspan=tspan, diff_par=diff_par, passengers_by=passengers_by)
     p = Pool(cpu_cores)
-    res = p.amap(tro.signature, tro.trajectories, tro.parameters)
+    if nsims == 1:
+        trajectories = [trajectories]
+    else:
+        trajectories = trajectories
+    res = p.amap(tro.signature, trajectories, parameters)
     return res.get()
