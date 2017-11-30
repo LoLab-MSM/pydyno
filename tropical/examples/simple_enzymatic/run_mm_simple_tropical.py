92dade50d7a68930e2b107d68d35520a5861452b
oscar <oo.ortega2247@gmail.com>
2017-11-30 15:15:26 -0600

    working enzymatic reaction example

--- tropical/examples/simple_enzymatic/run_mm_simple_tropical.py
+++ tropical/examples/simple_enzymatic/run_mm_simple_tropical.py
@@ -1,8 +1,7 @@
 import numpy as np
-
+from tropical.dynamic_signatures_range import run_tropical
 from pysb.simulator.scipyode import ScipyOdeSimulator
-from tropical.dynamic_signatures import run_tropical
-from tropical.examples import model
+from pysb.examples.tyson_oscillator import model
 
 tspan = np.linspace(0, 50, 501)
 sim = ScipyOdeSimulator(model, tspan=tspan).run()
