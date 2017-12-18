put sign to txt function in tools. Fixed tyson example with new bng notation for source and sink

tropical/examples/tyson_oscillator/tyson_oscillator_time_parameters_changed.py
@@ -5,4 +5,4 @@ from tropical.tools.miscellaneous_analysis import change_parameter_in_time
 
 tspan = np.linspace(0, 200, 200)
 
-change_parameter_in_time(model, tspan, 51, 5, ['kp4'], 2)
+change_parameter_in_time(model, tspan, 51, 4, ['kp4'], 2)