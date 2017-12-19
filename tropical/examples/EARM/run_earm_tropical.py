1b188aa78d70802d71cf5d4034db3fa2aea41637
oscar <oo.ortega2247@gmail.com>
2017-12-19 13:35:29 -0600

    cleaning up earm examples

--- tropical/examples/EARM/run_earm_tropical.py
+++ tropical/examples/EARM/run_earm_tropical.py
@@ -1,15 +1,10 @@
-import os
-import tropical.helper_functions as hf
 import numpy as np
-from earm.lopez_embedded import model
+from earm2_flat import model
 from tropical.dynamic_signatures_range import run_tropical
 from pysb.simulator.scipyode import ScipyOdeSimulator
 
-directory = os.path.dirname(__file__)
-parameters_path = os.path.join(directory, "parameters_5000")
-all_parameters = hf.listdir_fullpath(parameters_path)
-parameters = hf.read_pars(all_parameters[237])
+parameters = np.load('calibrated_6572pars.npy')
 
 t = np.linspace(0, 20000,  100)
-sim = ScipyOdeSimulator(model, tspan=t, param_values=parameters).run()
+sim = ScipyOdeSimulator(model, tspan=t, param_values=parameters[0]).run()
 run_tropical(model, simulations=sim, diff_par=1)
--- tropical/examples/EARM/run_earm_tropical_multi.py
+++ tropical/examples/EARM/run_earm_tropical_multi.py
@@ -1,16 +1,8 @@
-import os
-import numpy as np
-import tropical.helper_functions as hf
 import pickle
 from tropical.dynamic_signatures_range import run_tropical_multi
-from pysb.simulator.base import SimulationResult
-from earm.lopez_embedded import model
+from earm2_flat import model
 
-t = np.linspace(0, 20000, 100)
-
-#getting simulations
-sim = SimulationResult.load('simulations_earm6572.h5')
-a=run_tropical_multi(model, simulations=sim, cpu_cores=4)
+a = run_tropical_multi(model, simulations='earm_scipyode_sims.h5', cpu_cores=4)
 
 with open('earm_signatures.pickle', 'wb') as handle:
     pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
