48cb52171cf77b3f169f7956c56b2afb6b146814
oscar <oo.ortega2247@gmail.com>
2017-12-19 12:03:06 -0600

    add cluster labels

--- tropical/examples/EARM/cluster_drivers_signatures.py
+++ tropical/examples/EARM/cluster_drivers_signatures.py
@@ -5,11 +5,11 @@ from pathos.multiprocessing import ProcessingPool as Pool
 with open('earm_signatures.pickle', 'rb') as handle:
     all_signatures = pickle.load(handle)
 
-sil_threshold = 0.05
+sil_threshold = 0.025
 
 def get_cluster_percentage_color(signatures_idx):
     signatures = all_signatures[signatures_idx]['consumption']
-    clus = clustering.ClusterSequences(data=signatures, unique_sequences=False, truncate_seq=50)
+    clus = clustering.ClusterSequences(seqdata=signatures, unique_sequences=False, truncate_seq=50)
     clus.diss_matrix(n_jobs=4)
     sil_df = clus.silhouette_score_kmeans_range(cluster_range=range(2, 31), n_jobs=4, random_state=1234)
     if sil_threshold:
@@ -21,9 +21,8 @@ def get_cluster_percentage_color(signatures_idx):
         best_silh, n_clus = sil_df.loc[sil_df['cluster_silhouette'].idxmax()]
     n_clus = int(n_clus)
     clus.Kmeans(n_clusters=n_clus, random_state=1234)
-    cluster_information = {}
-    cluster_information[signatures_idx] = clus.cluster_percentage_color()
-    cluster_information['best_silh'] = best_silh
+    cluster_information = {signatures_idx: clus.cluster_percentage_color(),
+                           'best_silh': best_silh, 'labels': clus.labels}
     return cluster_information
 
 
@@ -33,5 +32,5 @@ p = Pool(4)
 res = p.amap(get_cluster_percentage_color, drivers)
 results = res.get()
 
-with open('results.pickle', 'wb') as fp:
+with open('results3.pickle', 'wb') as fp:
     pickle.dump(results, fp)
