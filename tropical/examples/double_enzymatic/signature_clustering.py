ec72cb7e12e2c22f48347f9607e50d45991b4ebc
oscar <oo.ortega2247@gmail.com>
2017-12-30 16:42:37 -0600

    signature clustering example

--- tropical/examples/double_enzymatic/signature_clustering.py
+++ tropical/examples/double_enzymatic/signature_clustering.py
@@ -5,7 +5,7 @@ with open('signatures.pickle', 'rb') as handle:
 
 sp0_sign_reactants = all_signatures[0]['consumption']
 clus = clustering.ClusterSequences(sp0_sign_reactants, unique_sequences=False)
-clus.diss_matrix()
-clus.Kmeans(2)
+clus.diss_matrix(n_jobs=1)
+clus.agglomerative_clustering(2)
 pl = clustering.PlotSequences(clus)
 pl.all_trajectories_plot()
