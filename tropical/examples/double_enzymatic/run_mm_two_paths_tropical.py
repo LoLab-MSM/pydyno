5fbc03cc4ad22ff2a64610f481623e6eb7390b3c
oscar <oo.ortega2247@gmail.com>
2018-01-03 14:21:35 -0600

    working double enzymatic reaction example

--- tropical/examples/double_enzymatic/run_mm_two_paths_tropical.py
+++ tropical/examples/double_enzymatic/run_mm_two_paths_tropical.py
@@ -1,11 +1,8 @@
 import numpy as np
-from tropical.dynamic_signatures_range import run_tropical_multi
+from tropical.dynamic_signatures_range import run_tropical
 from tropical.examples.double_enzymatic.mm_two_paths_model import model
 from pysb.simulator.scipyode import ScipyOdeSimulator
 
-parameters = np.load('calibrated_pars.npy')
-parameters = parameters
-
-tspan = np.linspace(0, 50, 101)
-sim = ScipyOdeSimulator(model, tspan=tspan).run(param_values=parameters)
-a = run_tropical_multi(model, simulations=sim, passengers_by='imp_nodes', diff_par=0.5)
+tspan = np.linspace(0, 10, 51)
+sim = ScipyOdeSimulator(model, tspan=tspan).run()
+a = run_tropical(model, simulations=sim, passengers_by='imp_nodes', diff_par=0.5, sp_to_vis=[0])
