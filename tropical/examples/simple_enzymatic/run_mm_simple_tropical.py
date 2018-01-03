f0425de0eb6ecacc320781c537acf1e1f9f72111
oscar <oo.ortega2247@gmail.com>
2018-01-03 12:52:57 -0600

    deleted simple enzymatic folder

--- tropical/examples/simple_enzymatic/run_mm_simple_tropical.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from tropical.dynamic_signatures_range import run_tropical
-from pysb.simulator.scipyode import ScipyOdeSimulator
-from pysb.examples.tyson_oscillator import model
-
-tspan = np.linspace(0, 50, 501)
-sim = ScipyOdeSimulator(model, tspan=tspan).run()
-run_tropical(model, simulations=sim, diff_par=0.5)
