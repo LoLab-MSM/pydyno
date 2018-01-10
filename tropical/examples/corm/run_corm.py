0d947f894e8eb2d034c7e822d133f3b4ed2748fd
oscar <oo.ortega2247@gmail.com>
2018-01-10 16:29:10 -0600

    run corm fixed

--- tropical/examples/corm/run_corm.py
+++ tropical/examples/corm/run_corm.py
@@ -9,7 +9,6 @@ t = np.linspace(0, 10, num=100)
 param_names = ['kcat_AA2', 'kcat_AA3', 'KD_AG_cat3', 'KD_AG_cat2', 'KD_AG_allo2', 'KD_AG_allo1', 'KD_AA_allo1',
                'KD_AA_allo2', 'KD_AA_allo3', 'kcat_AG3', 'KD_AA_cat3', 'KD_AA_cat2']
 
-calibrated_parameters = np.load('2015_02_02_COX2_all_traces.npy')
 solver = ScipyOdeSimulator(model, tspan=t)
 
 # def transform_parameters(pars):
@@ -20,6 +19,7 @@ solver = ScipyOdeSimulator(model, tspan=t)
 #
 #
 # p = Pool(4)
+# calibrated_parameters = np.load('2015_02_02_COX2_all_traces.npy')
 # unique_pars, unique_idx = np.unique(calibrated_parameters, axis=0, return_index=True)
 # pars_ready = p.map(transform_parameters, unique_pars)
 # pars_ready = np.array(pars_ready)
