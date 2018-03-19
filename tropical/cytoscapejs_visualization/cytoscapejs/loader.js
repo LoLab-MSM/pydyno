687fe2623383ef4901bb0880badc7cdbb1c2143f
oscar <oo.ortega2247@gmail.com>
2018-03-19 14:54:41 -0500

    tips using tippy

--- tropical/cytoscapejs_visualization/cytoscapejs/loader.js
+++ tropical/cytoscapejs_visualization/cytoscapejs/loader.js
@@ -3,12 +3,10 @@ if (window['cytoscape'] === undefined) {
     requirejs.config({
 
         paths: {
-            'cytoscape': 'https://cdnjs.cloudflare.com/ajax/libs/cytoscape/2.7.23/cytoscape.min',
-            'cytoscape-qtip': 'https://cdn.rawgit.com/cytoscape/cytoscape.js-qtip/2.7.0/cytoscape-qtip',
+            'cytoscape': 'https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.2.10/cytoscape',
             'cytoscape-popper': 'https://cdn.rawgit.com/cytoscape/cytoscape.js-popper/3ad50859/cytoscape-popper',
-			'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min',
-            'qtip2': 'https://cdnjs.cloudflare.com/ajax/libs/qtip2/2.2.0/basic/jquery.qtip.min',
-            'popper': 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper'
+            'popper': 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.1/umd/popper.min',
+            'tippy': 'https://cdnjs.cloudflare.com/ajax/libs/tippy.js/2.3.0/tippy.min'
         },
         shim: {
             'cytoscape-popper':{
@@ -17,11 +15,11 @@ if (window['cytoscape'] === undefined) {
         }
     });
     window.$ = window.jQuery = require('jquery');
-
-    requirejs(['cytoscape', 'cytoscape-qtip', 'cytoscape-popper', 'popper', 'jquery', 'qtip2'],
-        function (cytoscape, cyqtip, cypopper, popper, jquery) {
+    requirejs(['cytoscape', 'cytoscape-popper', 'popper', 'tippy'],
+        function (cytoscape, cypopper, popper, tippy) {
             console.log('Loading Cytoscape.js Module...');
-            cyqtip(cytoscape, jquery);
+            window['popper'] = popper;
+            window['tippy'] = tippy;
             cypopper(cytoscape);
             window['cytoscape'] = cytoscape;
 
