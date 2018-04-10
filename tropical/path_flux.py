54c432adf5761bffa82d953200f01b651024986d
oscar <oo.ortega2247@gmail.com>
2018-04-09 23:58:00 -0500

    path flux

--- tropical/path_flux.py
+++ tropical/path_flux.py
@@ -4,7 +4,60 @@ import sympy
 from pysb.simulator import ScipyOdeSimulator
 import numpy as np
 from pysb.pattern import SpeciesPatternMatcher
+from pysb.bng import generate_equations
 from pysb import ANY
+import networkx as nx
+import re
+
+
+def create_bipartite_graph(model):
+    generate_equations(model)
+    graph = nx.DiGraph(name=model.name)
+    ic_species = [cp for cp, parameter in model.initial_conditions]
+    for i, cp in enumerate(model.species):
+        species_node = 's%d' % i
+        slabel = re.sub(r'% ', r'%\\l', str(cp))
+        slabel += '\\l'
+        color = "#ccffcc"
+        # color species with an initial condition differently
+        if len([s for s in ic_species if s.is_equivalent_to(cp)]):
+            color = "#aaffff"
+        graph.add_node(species_node,
+                       label=slabel,
+                       shape="Mrecord",
+                       fillcolor=color, style="filled", color="transparent",
+                       fontsize="12",
+                       margin="0.06,0")
+    for i, reaction in enumerate(model.reactions_bidirectional):
+        reaction_node = 'r%d' % i
+        graph.add_node(reaction_node,
+                       label=reaction_node,
+                       shape="circle",
+                       fillcolor="lightgray", style="filled", color="transparent",
+                       fontsize="12",
+                       width=".3", height=".3", margin="0.06,0")
+        reactants = set(reaction['reactants'])
+        products = set(reaction['products'])
+        modifiers = reactants & products
+        reactants = reactants - modifiers
+        products = products - modifiers
+        attr_reversible = {'dir': 'both', 'arrowtail': 'empty'} if reaction['reversible'] else {}
+        for s in reactants:
+            r_link(graph, s, i, **attr_reversible)
+        for s in products:
+            r_link(graph, s, i, _flip=True, **attr_reversible)
+        for s in modifiers:
+            r_link(graph, s, i, arrowhead="odiamond")
+    return graph
+
+
+def r_link(graph, s, r, **attrs):
+    nodes = ('s%d' % s, 'r%d' % r)
+    if attrs.get('_flip'):
+        del attrs['_flip']
+        nodes = reversed(nodes)
+    attrs.setdefault('arrowhead', 'normal')
+    graph.add_edge(*nodes, **attrs)
 
 
 def get_edges_name(reactants, products):
@@ -26,12 +79,12 @@ def get_reaction_flux_df(model, tspan, param_values=None):
     else:
         # create parameter vector from the values in the model
         param_values = np.array([p.value for p in model.parameters])
-
-    rxns_df = pd.DataFrame(columns=tspan, index=range(len(model.reactions)))
+    rxns_names = ['r{0}'.format(rxn) for rxn in range(len(model.reactions_bidirectional))]
+    rxns_df = pd.DataFrame(columns=tspan, index=rxns_names)
     param_dict = dict((p.name, param_values[i]) for i, p in enumerate(model.parameters))
     sim_result = ScipyOdeSimulator(model, tspan=tspan, param_values=param_dict).run()
     y_df = sim_result.all
-    for idx, reac in enumerate(model.reactions):
+    for idx, reac in enumerate(model.reactions_bidirectional):
         rate_reac = reac['rate']
         for p in param_dict:
             rate_reac = rate_reac.subs(p, param_dict[p])
@@ -44,12 +97,21 @@ def get_reaction_flux_df(model, tspan, param_values=None):
                 args[idx2] = param_dict[va.name]
         func = sympy.lambdify(variables, rate_reac, modules=dict(sqrt=np.lib.scimath.sqrt))
         react_rate = func(*args)
-        edge_name = get_edges_name(reac['reactants'], reac['products'])
-        rxns_df.rename(index={int(idx): edge_name}, inplace=True)
-        rxns_df.loc[edge_name] = react_rate
+        # edge_name = get_edges_name(reac['reactants'], reac['products'])
+        # rxns_df.rename(index={int(idx): edge_name}, inplace=True)
+        rxns_df.loc['r{0}'.format(idx)] = react_rate
     rxns_df['Total'] = rxns_df.sum(axis=1)
     return rxns_df
 
+
+def get_sp_nodes(path):
+    sp_nodes = [node for node in path if node.startswith('s')]
+    return sp_nodes
+
+
+def get_r_nodes(path):
+    r_nodes = [node for node in path if node.startswith('r')]
+    return r_nodes
 
 def get_paths_flux(model, tspan, param_values, paths, network, total_flux=True):
 
@@ -57,40 +119,67 @@ def get_paths_flux(model, tspan, param_values, paths, network, total_flux=True):
     inst_flux_df = pd.DataFrame(columns=tspan, index=range(len(paths)))
     paths_flux = {}
     for path_idx, path in enumerate(paths):
-        checked_path = check_path(model, path)
-        if not checked_path:
-            print ('path {0} is not allowed'.format(path_idx))
-            continue
+        # checked_path = check_path(model, path)
+        # if not checked_path:
+        #     print ('path {0} is not allowed'.format(path_idx))
+        #     continue
         if total_flux:
             path_flux = 1
-            for node in range(1, len(path)-1):
-                in_edges = network.in_edges(path[node])
-                out_edges = network.out_edges(path[node])
-                total_flux_in = np.sum([reaction_flux_df.filter(like=str(edge), axis=0)['Total'].values[0] for edge in in_edges])
-                total_flux_out = np.sum([reaction_flux_df.filter(like=str(edge), axis=0)['Total'].values[0] for edge in out_edges])
-                flux_in = np.sum(reaction_flux_df.filter(like=str((path[node-1], path[node])), axis=0)['Total'].values[0])
-                flux_out = np.sum(reaction_flux_df.filter(like=str((path[node], path[node+1])), axis=0)['Total'].values[0])
-                percentage_in = flux_in / total_flux_in
-                percentage_out = flux_out / total_flux_out
-                node_flux = percentage_in*flux_out*percentage_out
+            sp_nodes = get_sp_nodes(path)
+            r_nodes = get_r_nodes(path)
+            node_flux = 1
+            for sp_node, r_node in zip(sp_nodes[1:-1], r_nodes[:-1]):
+                in_edges = network.in_edges(sp_node)
+                out_edges = network.out_edges(sp_node)
+                total_flux_in_l = np.array([reaction_flux_df.loc[edge[0], 'Total'] for edge in in_edges])
+                total_flux_in = np.sum(total_flux_in_l[np.where(total_flux_in_l > 0)])
+                flux_in_negative = np.sum(total_flux_in_l[np.where(total_flux_in_l < 0)])
+                total_flux_out_l = np.array([reaction_flux_df.loc[edge[1], 'Total'] for edge in out_edges])
+                total_flux_out = np.sum(total_flux_out_l[np.where(total_flux_out_l > 0)])
+                flux_out_negative = np.sum(total_flux_out_l[np.where(total_flux_out_l < 0)])
+                total_flux_in += flux_out_negative * -1
+                total_flux_out += flux_in_negative * -1
+                # print ('in', sp_node, flux_in_negative)
+                # print('out', sp_node, flux_out_negative)
+
+                flux_in = np.sum(reaction_flux_df.loc[r_node, 'Total'])
+                next_r_node = r_nodes.index(r_node) + 1
+                flux_out = np.sum(reaction_flux_df.loc[r_nodes[next_r_node], 'Total'])
+                if total_flux_out < np.finfo(float).eps:
+                    percentage_out = 0
+                else:
+                    percentage_out = flux_out / total_flux_out
+
+                if total_flux_in < np.finfo(float).eps:
+                    percentage_in = 0
+                else:
+                    percentage_in = flux_in / total_flux_in
+
+                node_flux = percentage_in * total_flux_out * percentage_out
 
                 path_flux *= node_flux
+            if path_flux <= 0:
+                continue
             paths_flux[path_idx] = path_flux
         else:
             cum_path_flux = 0
+            sp_nodes = get_sp_nodes(path)
+            r_nodes = get_r_nodes(path)
             for t in tspan:
                 path_flux = 1
-                for node in range(1, len(path) - 1):
-                    in_edges = network.in_edges(path[node])
-                    out_edges = network.out_edges(path[node])
+                for sp_node, r_node in zip(sp_nodes[1:-1], r_nodes[:-1]):
+                    in_edges = network.in_edges(sp_node)
+                    out_edges = network.out_edges(sp_node)
+
                     total_flux_in = np.sum(
-                        [reaction_flux_df.filter(like=str(edge), axis=0)[t].values[0] for edge in in_edges])
+                        [reaction_flux_df.loc[edge[0], t] for edge in in_edges])
                     total_flux_out = np.sum(
-                        [reaction_flux_df.filter(like=str(edge), axis=0)[t].values[0] for edge in out_edges])
+                        [reaction_flux_df.loc[edge[1], t] for edge in out_edges])
                     flux_in = np.sum(
-                        reaction_flux_df.filter(like=str((path[node - 1], path[node])), axis=0)[t].values[0])
+                        reaction_flux_df.loc[r_node, t])
+                    next_r_node = r_nodes.index(r_node) + 1
                     flux_out = np.sum(
-                        reaction_flux_df.filter(like=str((path[node], path[node + 1])), axis=0)[t].values[0])
+                        reaction_flux_df.loc[r_nodes[next_r_node], t])
 
                     if total_flux_out < np.finfo(float).eps:
                         percentage_out = 0
