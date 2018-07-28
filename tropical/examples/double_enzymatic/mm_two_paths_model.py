22d0827c1414510852070368f0804a99df59a3aa
oscar <oo.ortega2247@gmail.com>
2018-07-27 22:10:46 -0500

    removing more dependences. Fix model

--- tropical/examples/double_enzymatic/mm_two_paths_model.py
+++ tropical/examples/double_enzymatic/mm_two_paths_model.py
@@ -14,30 +14,30 @@ Parameter('kcat2', 10.)
 
 
 Monomer('E', ['s'])
-Monomer('S', ['e', 'type'], {'type': ['1', '2']})
+Monomer('S', ['e', 'type'], {'type': ['A', 'B']})
 Monomer('P')
 
 # Rules
-Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='1') <> E(s=1) % S(e=1, type='1'), kf1, kr1)
-Rule('Production_1', E(s=1) % S(e=1, type='1') >> E(s=None) + P(), kcat1)
-Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='2') <> E(s=1) % S(e=1, type='2'), kf2, kr2)
-Rule('Production_2', E(s=1) % S(e=1, type='2') >> E(s=None) + P(), kcat2)
+Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='A') <> E(s=1) % S(e=1, type='A'), kf1, kr1)
+Rule('Production_1', E(s=1) % S(e=1, type='A') >> E(s=None) + P(), kcat1)
+Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='B') <> E(s=1) % S(e=1, type='B'), kf2, kr2)
+Rule('Production_2', E(s=1) % S(e=1, type='B') >> E(s=None) + P(), kcat2)
 
 # Macro
 # catalyze_state(E(), 's', S(), 'e', 'state', '0', '1', [kf,kr,kcat])
 
 Observable("E_free",      E(s=None))
-Observable("S1_free",     S(e=None, type='1'))
-Observable("S2_free",     S(e=None, type='2'))
-Observable("ES1_complex", E(s=1) % S(e=1, type='1'))
-Observable("ES2_complex", E(s=1) % S(e=1, type='2'))
+Observable("S1_free",     S(e=None, type='A'))
+Observable("S2_free",     S(e=None, type='B'))
+Observable("ES1_complex", E(s=1) % S(e=1, type='A'))
+Observable("ES2_complex", E(s=1) % S(e=1, type='B'))
 Observable("Product",     P())
 
 Parameter("Etot", 1.*V)
 Initial(E(s=None), Etot)
 
 Parameter('S1_0', 10.*V)
-Initial(S(e=None, type='1'), S1_0)
+Initial(S(e=None, type='A'), S1_0)
 
 Parameter('S2_0', 10.*V)
-Initial(S(e=None, type='2'), S2_0)
+Initial(S(e=None, type='B'), S2_0)
