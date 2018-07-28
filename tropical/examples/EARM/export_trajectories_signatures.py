08725537855c096acc9be7367f3b61316a9be87e
oscar <oo.ortega2247@gmail.com>
2018-07-27 21:55:38 -0500

    Fixed hdbscan dependence in cython

--- tropical/examples/EARM/export_trajectories_signatures.py
+++
@@ -1,6 +0,0 @@
-from tropical.tools.miscellaneous_analysis import trajectories_signature_2_txt
-import numpy as np
-from earm2_flat import model
-
-tspan = np.linspace(0, 20000, 100)
-trajectories_signature_2_txt(model, tspan=tspan, sp_to_analyze=[37], parameters=None)
--- tropical/examples/EARM/run_earm_tropical_multi.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from tropical.dynamic_signatures_range import run_tropical_multi
-from earm2_flat import model
-
-a = run_tropical_multi(model, simulations='earm_scipyode_sims.h5', cpu_cores=4)
-
-with open('earm_signatures.pickle', 'wb') as handle:
-    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
