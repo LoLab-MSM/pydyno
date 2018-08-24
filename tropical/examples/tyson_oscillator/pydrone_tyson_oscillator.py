b3a0361b3210ff19b921b4d7d5cccab53671410e
oscar <oo.ortega2247@gmail.com>
2018-08-24 15:49:15 -0500

    complete tyson oscillator example

--- tropical/examples/tyson_oscillator/pydrone_tyson_oscillator.py
+++ tropical/examples/tyson_oscillator/pydrone_tyson_oscillator.py
@@ -1,4 +1,4 @@
-from run_tyson_oscillator import model
+from pysb.examples.tyson_oscillator import model
 import numpy as np
 from pysb.simulator import ScipyOdeSimulator
 from tropical.pydrone import Pydrone
@@ -9,3 +9,7 @@ pars2 = [pars1, pars1, pars1/2, pars1/2]
 sims = ScipyOdeSimulator(model, tspan=tspan).run(param_values=pars2)
 
 pyd = Pydrone(model, sims, diff_par=1)
+pyd.discretize(2)
+print(pyd.sequences.loc['__s2_p'])
+pyd.cluster_signatures_spectral('__s2_c', nclusters=3, cluster_range=True)
+pyd.analysis_cluster.plot_cluster_dynamics([2], norm=True)
