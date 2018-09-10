30ecb13940601a3c429927b46133e3c1ee9fb4fb
oscar <oo.ortega2247@gmail.com>
2018-09-10 14:40:04 -0500

    Added anytree to setup to fix travis. Removed unused files

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ else:
                               libraries=math_lib,
                               include_dirs=py_inc + np_inc)]
 
-install_requires = ['pysb', 'seaborn', 'scikit-learn', 'editdistance', 'pandas', 'future']
+install_requires = ['pysb', 'seaborn', 'anytree', 'scikit-learn', 'editdistance', 'pandas', 'future']
 
 setup(name='DynSign',
       version='1.0',
--- tropical/examples/earm/path_flux_analysis.py
+++ tropical/examples/earm/path_flux_analysis.py
@@ -1,4 +1,4 @@
-from tropical.dominant_path_analysis import run_dompath_multi, run_dompath_single
+from tropical.discretize_path import DomPath
 from tropical.examples.earm.earm2_flat import model
 import numpy as np
 from pysb.simulator import ScipyOdeSimulator
@@ -10,4 +10,5 @@ tspan = np.linspace(0, 20000, 1000)
 # data = viz.static_view(get_passengers=False)
 sim = ScipyOdeSimulator(model, tspan).run(param_values=pars[0])
 # dompath = run_dompath_multi(sim, ref=100, target='s27', depth=3, cpu_cores=2)
-d = run_dompath_single(model, sim, target='s65', depth=12, dom_om=0.5)
+dis = DomPath(model, sim, target='s65', depth=7, dom_om=0.5)
+signatures, paths = dis.get_path_signatures(cpu_cores=2)
