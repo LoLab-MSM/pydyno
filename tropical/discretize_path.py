9552bb9a3294a811440b92b6ec864ac61d40615a
Blake <blakeaw1102@gmail.com>
2018-09-12 18:18:58 -0500

    Finished a first path of a conserved species analysis function using dominant paths and the path signatures.

--- tropical/discretize_path.py
+++ tropical/discretize_path.py
@@ -9,6 +9,7 @@ import tropical.util as hf
 from collections import defaultdict, OrderedDict
 from anytree import Node, findall
 from anytree.exporter import DictExporter
+from anytree.importer import DictImporter
 from collections import ChainMap
 import time
 try:
@@ -383,3 +384,75 @@ def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)
+
+def get_path_descendants(path):
+    """ Get the set of descendants for a tree like path dict.
+    """
+
+    importer = DictImporter()
+    root = importer.import_(path)
+    descendants = set([descendant_node.name for descendant_node in root.descendants])
+    return descendants
+
+def global_conserved_species_analysis(paths, path_signatures, model):
+    """
+    Computes the fraction of dominant paths a species is in. It is taken over
+        all simulations and all timepoints.
+    Parameters
+    ----------
+    paths: dict
+        Nested tree structure dict of paths as returned from
+            DomPath.get_path_signatures()
+    path_signatures: pandas.DataFrame
+        The dominant path signatures for each simulation (across all
+            timepoints).
+    model: pysb.Model
+        The model that is being used.
+
+    Returns
+    -------
+    A list of tuples with the species codename
+        (i.e. 's' + str( model.species_index)) and the fraction of dominant
+        paths that species was in.
+    """
+
+    generate_equations(model)
+    species_all = model.species
+    #print(species_all)
+    n_species_all = len(species_all)
+    spec_dict = dict()
+    spec_counts = np.array([0.0] * n_species_all)
+    #species_all_snames = []
+    for i, species in enumerate(species_all):
+        sname = "s{}".format(i)
+        spec_dict[sname] = {'name': species, 'index': i}
+
+    path_species = dict()
+    for i, key in enumerate(paths.keys()):
+        path = paths[key]
+        descendants = get_path_descendants(path)
+        #print(descendants)
+        path_species[i] = descendants
+    path_signatures_np = path_signatures.values
+    n_sims = path_signatures_np.shape[0]
+    n_tp = path_signatures_np.shape[1]
+    #print(n_sims, n_tp)
+    #quit()
+    n_tot = 0.0
+    for i in range(n_sims):
+        for j in range(n_tp):
+            n_tot += 1.0
+            dom_path_id = path_signatures_np[i][j]
+            #print(dom_path_id)
+            for descendant in path_species[dom_path_id]:
+            #    print(descendant)
+                d_id = spec_dict[descendant]['index']
+                spec_counts[d_id] += 1.0
+    #print(n_tot)
+    spec_fracs = spec_counts / n_tot
+    #quit()
+    spec_frac_dict = dict()
+    for spec in spec_dict.keys():
+        spec_frac_dict[spec] = spec_fracs[spec_dict[spec]['index']]
+    sorted_by_value = sorted(spec_frac_dict.items(), key=lambda kv: -kv[1])
+    return sorted_by_value
