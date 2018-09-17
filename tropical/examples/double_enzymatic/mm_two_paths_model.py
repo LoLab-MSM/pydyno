2c803edaced344eba6fa58f90c319fe03a3da9f1
oscar <oo.ortega2247@gmail.com>
2018-09-17 14:42:45 -0500

    Updating pysb syntaxis in model

--- tropical/examples/double_enzymatic/mm_two_paths_model.py
+++ tropical/examples/double_enzymatic/mm_two_paths_model.py
@@ -18,9 +18,9 @@ Monomer('S', ['e', 'type'], {'type': ['A', 'B']})
 Monomer('P')
 
 # Rules
-Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='A') <> E(s=1) % S(e=1, type='A'), kf1, kr1)
+Rule('ReversibleBinding_1', E(s=None) + S(e=None, type='A') | E(s=1) % S(e=1, type='A'), kf1, kr1)
 Rule('Production_1', E(s=1) % S(e=1, type='A') >> E(s=None) + P(), kcat1)
-Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='B') <> E(s=1) % S(e=1, type='B'), kf2, kr2)
+Rule('ReversibleBinding_2', E(s=None) + S(e=None, type='B') | E(s=1) % S(e=1, type='B'), kf2, kr2)
 Rule('Production_2', E(s=1) % S(e=1, type='B') >> E(s=None) + P(), kcat2)
 
 # Macro
