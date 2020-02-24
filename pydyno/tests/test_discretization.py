Fix expression calculation for 2d trajectories

pydyno/tests/test_discretization.py
@@ -97,7 +97,7 @@ class TestPathPysbSingle:
 
     def test_expr_in_model(self, pysb_dom_expr_path):
         expr = pysb_dom_expr_path[1].model.expressions[0]
-        tr = pysb_dom_expr_path[1].trajectories
+        tr = pysb_dom_expr_path[1].trajectories[0]
         param_dict = {p.name: p.value for p in pysb_dom_expr_path[1].model.parameters}
         expr_sim = pysb_dom_expr_path[1]._calculate_expression(expr, tr, param_dict)
         assert np.allclose(expr_sim, pysb_dom_expr_path[0].all[expr.name])